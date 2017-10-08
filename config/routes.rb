760f7e39cd3630c35dd63de8b9a48f25cdba428e
Alessandro Descovi <descovi@gmail.com>
2017-10-08 11:45:26 +0200

    rimozione spazi vuoti

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :spid
-  get 'metadata', to: 'spid#metadata'
-  
+  get  'metadata', to: 'spid#metadata'
   post 'print_response', to: 'spid#print_response'
   root to: 'spid#index'
 end
