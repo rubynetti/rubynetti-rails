852f74635af9aed8523b372005aec8cd6ae4356b
Alessandro Descovi <descovi@gmail.com>
2017-10-08 10:59:23 +0200

    rimosso raise false

--- app/controllers/spid_controller.rb
+++ app/controllers/spid_controller.rb
@@ -1,45 +1,45 @@
 
 class SpidController < ApplicationController
-  skip_before_action :verify_authenticity_token, raise: false, only: :print_response
+  skip_before_action :verify_authenticity_token, only: :print_response
 
-    def index
-      parser = OneLogin::RubySaml::IdpMetadataParser.new
+  def index
+    parser = OneLogin::RubySaml::IdpMetadataParser.new
 
-      # settings = parser.parse_remote("http://spidposte.test.poste.it/jod-fs/metadata/idp.xml", true, {
-      settings = parser.parse_remote("http://localhost:3000/metadata_idp", true, {
-        sso_binding: ["urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"]
-      })
+    # settings = parser.parse_remote("http://spidposte.test.poste.it/jod-fs/metadata/idp.xml", true, {
+    settings = parser.parse_remote("http://localhost:3000/metadata_idp", true, {
+      sso_binding: ["urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"]
+    })
 
-      # Dati a cui IDP farà riferimento per rispondere
-      settings.assertion_consumer_service_url = "http://localhost:3000/print_response"
-      settings.assertion_consumer_service_binding = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
+    # Dati a cui IDP farà riferimento per rispondere
+    settings.assertion_consumer_service_url = "http://localhost:3000/print_response"
+    settings.assertion_consumer_service_binding = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
 
-      settings.issuer        = metadata_url
-      settings.authn_context = 'https://www.spid.gov.it/SpidL1'
-
-
-      request = OneLogin::RubySaml::Authrequest.new
-      output = request.create(settings)
-      redirect_to output
-    end
-
-    def print_response
-      render html: params.inspect
-    end
+    settings.issuer        = metadata_url
+    settings.authn_context = 'https://www.spid.gov.it/SpidL1'
 
 
-    # Metadata locale
-    def metadata
-      metadata = OneLogin::RubySaml::Metadata.new
-      settings = OneLogin::RubySaml::Settings.new
-      settings.assertion_consumer_service_url = "http://localhost:3000/print_response"
-      settings.assertion_consumer_service_binding = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
-      settings.issuer = metadata_url
-      render xml: metadata.generate(settings)
-    end
+    request = OneLogin::RubySaml::Authrequest.new
+    output = request.create(settings)
+    redirect_to output
+  end
 
-    def metadata_idp
-      render xml: File.open("app/views/spid/metadata_idp.xml")
-    end
+  def print_response
+    render html: params.inspect
+  end
+
+
+  # Metadata locale
+  def metadata
+    metadata = OneLogin::RubySaml::Metadata.new
+    settings = OneLogin::RubySaml::Settings.new
+    settings.assertion_consumer_service_url = "http://localhost:3000/print_response"
+    settings.assertion_consumer_service_binding = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"
+    settings.issuer = metadata_url
+    render xml: metadata.generate(settings)
+  end
+
+  def metadata_idp
+    render xml: File.open("app/views/spid/metadata_idp.xml")
+  end
 
 end
