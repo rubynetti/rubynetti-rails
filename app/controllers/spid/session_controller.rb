dd2a7d6b7282eddf9dc87c0337b96c1212ab8317
Alessandro Descovi <descovi@gmail.com>
2017-10-08 13:54:13 +0200

    fake gov

--- app/controllers/spid/session_controller.rb
+++ app/controllers/spid/session_controller.rb
@@ -5,7 +5,7 @@ class Spid::SessionController < ApplicationController
     parser = OneLogin::RubySaml::IdpMetadataParser.new
 
     #settings = parser.parse_remote("http://spidposte.test.poste.it/jod-fs/metadata/idp.xml", true, {
-    settings = parser.parse_remote("http://localhost:3000/metadata-idp-fake.xml", true, {
+    settings = parser.parse_remote("http://localhost:3000/metadata-idp-fake-gov.xml", true, {
       sso_binding: ["urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect"]
     })
 
