0d0555df8d67cc0b53696cd6377a58b92f615b89
Jecko-o <tunedstoned@yahoo.it>
2017-10-08 12:19:13 +0200

    refactor spid routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :spid do
-    resource :metadata
-    resources :session
+    resource :metadata, only: :show
+    resources :session, only: [:new, :create]
   end
-  #get  'metadata', to: 'spid#metadata'
-  #post 'print_response', to: 'spid#print_response'
   root to: 'spid/session#new'
 end
